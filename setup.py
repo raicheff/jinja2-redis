454e396d06eeab49ef69b868c236c7fe752e9b36
Boris Raicheff <b@raicheff.com>
2018-06-30 16:39:23 +0100

    Fix module name in setup.py

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ from setuptools import setup
 
 setup(
     name='Jinja2-Redis',
-    version='0.1.1',
+    version='0.1.2',
     description='Jinja2-Redis',
     author='Boris Raicheff',
     author_email='b@raicheff.com',
     url='https://github.com/raicheff/jinja2-redis',
-    py_modules=('jinja2_minify',),
+    py_modules=('jinja2_redis',),
 )
 
 
